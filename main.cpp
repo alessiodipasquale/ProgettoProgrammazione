b90c2f843bae3f8c67a86420ed466991fda8243c
Alessio Di Pasquale <alessiodipasquale2000@gmail.com>
2020-08-06 22:47:33 +0200

    Started bad menu :(

--- main.cpp
+++ main.cpp
@@ -42,19 +42,103 @@ class Game {
         int mapHeight;
         int mapWidth;
 
-        void prepare(){
+        void cleanScreen() {
+            system("clear");
+        }       
 
+        void mainMenu() {
+            bool valid = false;
+            while(!valid){
+                cleanScreen();
+                cout<<"********************************************************************"<<endl;
+                cout<<"*                       Progetto programmazione                    *"<<endl;
+                cout<<"*                                                                  *"<<endl;
+                cout<<"*                                                                  *"<<endl;
+                cout<<"*                                 ^                                *"<<endl;
+                cout<<"*                               < P >                              *"<<endl;
+                cout<<"*                                 v                                *"<<endl;
+                cout<<"*                                                                  *"<<endl;
+                cout<<"*                                                                  *"<<endl;
+                cout<<"*                         1. Avvia il gioco                        *"<<endl;
+                cout<<"*                                                                  *"<<endl;
+                cout<<"*                      2. Informazioni e crediti                   *"<<endl;
+                cout<<"*                                                                  *"<<endl;
+                cout<<"*                              3. Esci                             *"<<endl;
+                cout<<"*                                                                  *"<<endl;
+                cout<<"********************************************************************"<<endl;
+                cout<<endl;
+                cout<<"Seleziona: ";
+                int risposta;
+                cin>>risposta;
+                switch (risposta)
+                {
+                case 1:
+                    valid = true;
+                    prepare();
+                    break;
+                case 2:
+                    valid = true;
+                    information();
+                    break;
+                case 3:
+                    valid = true;
+                    void kill();
+                default:
+                    break;
+                }
+            }
+        }
+        void information() {
+            bool valid = false;
+            while(!valid){
+                cleanScreen();
+                cout<<"********************************************************************"<<endl;
+                cout<<"*                        Progetto programmazione                   *"<<endl;
+                cout<<"*                                                                  *"<<endl;
+                cout<<"*                               Autori                             *"<<endl;
+                cout<<"*                         Di Pasquale Alessio                      *"<<endl;
+                cout<<"*                         Colamonaco Stefano                       *"<<endl;
+                cout<<"*                                                                  *"<<endl;
+                cout<<"*               (So che non e' centrato, giuro che fixo)           *"<<endl;
+                cout<<"*                                                                  *"<<endl;
+                cout<<"*                           1. Torna al menu                       *"<<endl;
+                cout<<"*                               2. Esci                            *"<<endl;
+                cout<<"********************************************************************"<<endl;
+                cout<<endl;
+                cout<<"Seleziona: ";
+                int risposta;
+                cin>>risposta;
+                switch (risposta)
+                {
+                case 1:
+                    valid = true;
+                    mainMenu();
+                    break;
+                case 2:
+                    valid = true;
+                    kill();
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+        void prepare(){
+            cleanScreen();
+            cout<<"Preparo il gioco...";
         }
         void start(){
 
         } 
+        int kill() {
+            return 0;
+        }
 };
 
 int main() {
     //Qui va il menù principale, dove metterei logo, play, info (con una legenda di simboli e punteggi), credits, exit, 
     
     Game game;
-    game.prepare();
-    game.start();
+    game.mainMenu();
     return 0;
 }
---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout<<"Ao";
+    return 0;
+}
